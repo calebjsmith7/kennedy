b1588aafd2daf3ca61c3ac9619a0b80c6f5e66d7
Caleb Smith <calebsmith@Calebs-MacBook-Pro-2.local>
2021-04-09 16:49:19 -0600

    gallery update

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,8 +20,21 @@ class Home extends Component {
                 <h1 className="info">At Kennedy Racecars <br/> we specialize in providing the highest quality <br/>
                 Restorations, Modifications, and Race Cars <br/>
                 and always exceed expectations</h1>
-    
+            <hr/>
             </div>
+            <div className="scrollsection"><h1 className="gallery">Gallery</h1></div>
+            <div className="scrolling-wrapper">
+                <div className="card slide1"></div>
+                <div className="card slide2"></div>
+                <div className="card slide3"></div>
+                <div className="card slide4"></div>
+                <div className="card slide5"></div>
+                <div className="card slide6"></div>
+                <div className="card slide7"></div>
+            </div>
+            <div className="bottomscroll"></div>
+        
+
             <div className="c">
                 <a id="mods">
                     <h3 className="mods">CUSTOM MODS</h3>
