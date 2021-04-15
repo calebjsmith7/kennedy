f3e600a1e72de729a29f6f9b4397d496a466121f
Caleb Smith <calebsmith@Calebs-MacBook-Pro-2.local>
2021-04-15 16:13:40 -0600

    mobile friendly lowernav

--- src/components/Lowermenu.js
+++ src/components/Lowermenu.js
@@ -12,19 +12,23 @@ class Lowermenu extends Component {
         <div className="lower">
           <div className="bottomlogo"></div>
           <div className="cw">
-            <h1 className="cw">© 2021 KENNEDY RACECARS • 1950 WEST UNION AVENUE A-10, ENGLEWOOD, CO 80110 • 303.781.7278</h1>
+            <h1 className="cw">© 2021 KENNEDY RACECARS • 1950 WEST UNION AVENUE A-10, ENGLEWOOD, CO 80110</h1>
           </div>
           <div className="bottomright">
             <ul className="bottomlinks">
-              <li><a href='/#restorations' className="fix">RESTORATIONS</a></li>
-              <li>|</li>
-              <li><a href='/#mods' className="fix">MODS</a></li>
-              <li>|</li>
-              <li><a href='/#racecars' className="fix">RACE CARS</a></li>
-              <li>|</li>
-              <li><Link to='/services' className="fix">ABOUT US</Link></li>
-              <li>|</li>
-              <li><Link to='/contact' className="fix">CONTACT</Link></li>
+              <li><a href='/#restorations' className="fix">Restorations</a></li>
+              <li className="vertsep">|</li>
+              <hr className="horizsep"></hr>
+              <li><a href='/#mods' className="fix">Mods</a></li>
+              <li className="vertsep">|</li>
+              <hr className="horizsep"></hr>
+              <li><a href='/#racecars' className="fix">Race Cars</a></li>
+              <li className="vertsep">|</li>
+              <hr className="horizsep"></hr>
+              <li><Link to='/services' className="fix">About Us</Link></li>
+              <li className="vertsep">|</li>
+              <hr className="horizsep"></hr>
+              <li><Link to='/contact' className="fix">Contact</Link></li>
             </ul>
           </div>
         </div>
