83cf3a9cf819577c4fb1d67ccaa97e695507dfda
Caleb <caleb@Calebs-new-Macbook-Pro.local>
2023-03-07 16:59:14 -0700

    feat. scroll behavior

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,9 +7,9 @@ import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 
 
-class Home extends Component {
+const Home = () => {
   
-  render() {
+ 
     return (
         <div>
             <a id="top"></a>
@@ -20,17 +20,19 @@ class Home extends Component {
                 <video playsInline webkit-playsinline autoPlay loop muted preload="true" id="smallvid" width="100%" className="video">
                 <source src={kensmallvid} type="video/mp4" media="(max-device-width: 568px)"></source>
                 </video>
+                <button style={{border: 'none', color: 'transparent', backgroundColor: 'transparent', outline: 'none'}} onClick={()=>window.scroll(0,document.querySelector('.b').getBoundingClientRect().top)}><h1 style={{position: 'absolute', top: '83%', left: '50%', zIndex: 1000, color: 'white', fontSize: 60}}>⌄</h1></button>
             </div>
             
             
             <div className="b">
                 <h1 className="info">At Kennedy Racecars we provide the highest quality
-                Restorations, Modifications, and Race Cars
+                Restorations, Modifications, and Race Cars,
                 and always exceed expectations</h1>
             <hr className="binfoh"/>
             </div>
             <div className="scrollsection"><h1 className="gallery">Gallery</h1></div>
-            <div className="scrolling-wrapper">
+            <div className="scrolling-wrapper" id="container">
+               
                 <div className="cards slide1"></div>
                 <div className="cards slide2"></div>
                 <div className="cards slide3"></div>
@@ -42,9 +44,14 @@ class Home extends Component {
                 <div className="cards slide9"></div>
                 <div className="cards slide10"></div>
                 <div className="cards slide11"></div>
-        
+                <div className="cards slide12"></div>
+                <div className="cards slide13"></div>
             </div>
-            <div className="bottomscroll"></div>
+        
+            <div className="bottomscroll">
+            <button style={{backgroundColor: 'transparent', color: 'white', border: 'none', fontSize: 30, marginRight: 30, outline: 'none'}} onClick={()=>document.getElementById('container').scrollLeft -= 250}>←</button>
+                <button style={{backgroundColor: 'transparent', color: 'white', border: 'none', fontSize: 30, outline: 'none'}} onClick={()=>document.getElementById('container').scrollLeft += 250}>→</button>
+            </div>
         
 
             <div className="c">
@@ -67,7 +74,6 @@ class Home extends Component {
             </div>
         </div>
     );
-  }
 }
 
 export default Home;
